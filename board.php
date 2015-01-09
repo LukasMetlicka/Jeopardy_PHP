3618a942f4c0573a008ca400fe965f400928cc12
Lukas Metlicka <lmetlicka@g.harvard.edu>
2015-01-09 09:48:00 -0500

    Added files I was stupid to not add

---
+++ board.php
@@ -0,0 +1,194 @@
+<?php
+
+    session_start();
+if (empty($_SESSION)){
+    $_SESSION = $_GET;
+}
+var_dump($_SESSION);
+?>
+<html>
+
+    <head>
+        <link href="style/board.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <h1>Jeopardy!</h1>
+        <div id="Green">
+
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="G100">
+                    <input type="submit" value="100">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="G200">
+                    <input type="submit" value="200">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="G300">
+                    <input type="submit" value="300">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="G400">
+                    <input type="submit" value="400">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="G500">
+                    <input type="submit" value="500">
+                </form>
+            </div>
+
+        </div>
+
+        <div id="Orange">
+
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="O100">
+                    <input type="submit" value="100">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="O200">
+                    <input type="submit" value="200">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="O300">
+                    <input type="submit" value="300">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="O400">
+                    <input type="submit" value="400">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="O500">
+                    <input type="submit" value="500">
+                </form>
+            </div>
+            
+        </div>
+
+        <div id="Blue">
+
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="B100">
+                    <input type="submit" value="100">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="B200">
+                    <input type="submit" value="200">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="B300">
+                    <input type="submit" value="300">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="B400">
+                    <input type="submit" value="400">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="B500">
+                    <input type="submit" value="500">
+                </form>
+            </div>
+            
+        </div>
+
+        <div id="Yellow">
+
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="Y100">
+                    <input type="submit" value="100">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="Y200">
+                    <input type="submit" value="200">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="Y300">
+                    <input type="submit" value="300">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="Y400">
+                    <input type="submit" value="400">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="Y500">
+                    <input type="submit" value="500">
+                </form>
+            </div>
+
+        </div>
+
+        <div id="Red">
+
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="R100">
+                    <input type="submit" value="100">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="R200">
+                    <input type="submit" value="200">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="R300">
+                    <input type="submit" value="300">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="R400">
+                    <input type="submit" value="400">
+                </form>
+            </div>
+            <div class="qdiv">
+                <form action="Slate.php" method="POST">
+                    <input type="hidden" name="button" value="R500">
+                    <input type="submit" value="500">
+                </form>
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,167 @@
+<!-- TODO LIST >
+    -> Add Answer input fields
+    -> Create Jeopardy playing field
+<! -->
+<html>
+
+    <head>
+        <link href="style/index.css" type="text/css" rel="stylesheet">
+    </head>
+
+    <body>
+        <form action="board.php" method="GET">
+
+            <div class="column" id="Green">
+                <h1 id="col1" >Column 1:</h1>
+                <div class="qdiv odd">
+                    <div class="text"> Green 100:</div>
+                    Q:<input class="question" type="text" name="q1:1"><br>
+                    A:<input class="answer" type="text" name="a1:1">
+                </div>
+                <div class="qdiv even">
+                    <div class="text"> Green 200:</div>
+                    Q:<input class="question" type="text" name="q1:2"><br>
+                    A:<input class="answer" type="text" name="a1:2">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text"> Green 300:</div>
+                    Q:<input class="question" type="text" name="q1:3"><br>
+                    A:<input class="answer" type="text" name="a1:3">
+                </div>
+                <div class="qdiv even">
+                    <div class="text"> Green 400:</div>
+                    Q:<input class="question" type="text" name="q1:4"><br>
+                    A:<input class="answer" type="text" name="a1:4">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text"> Green 500:</div>
+                    Q:<input class="question" type="text" name="q1:5"><br>
+                    A:<input class="answer" type="text" name="a1:5">
+                </div>
+            </div>
+
+            <div class="column" id="Orange">
+                <h1 id="col2" >Column 2:</h1>
+                <div class="qdiv odd">
+                    <div class="text">Orange 100:</div>
+                    Q:<input class="question" type="text" name="q2:1"><br>
+                    A:<input class="answer" type="text" name="a2:1">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Orange 200:</div>
+                    Q:<input class="question" type="text" name="q2:2"><br>
+                    A:<input class="answer" type="text" name="a2:2">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Orange 300:</div>
+                    Q:<input class="question" type="text" name="q2:3"><br>
+                    A:<input class="answer" type="text" name="a2:3">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Orange 400:</div>
+                    Q:<input class="question" type="text" name="q2:4"><br>
+                    A:<input class="answer" type="text" name="a2:4">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Orange 500:</div>
+                    Q:<input class="question" type="text" name="q2:5"><br>
+                    A:<input class="answer" type="text" name="a2:5">
+                </div>
+            </div>
+
+            <div class="column" id="Blue">
+                <h1 id="col3" >Column 3:</h1>
+                <div class="qdiv odd">
+                    <div class="text">Blue 100:</div>
+                    Q:<input class="question" type="text" name="q3:1"><br>
+                    A:<input class="answer" type="text" name="a3:1">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Blue 200:</div>
+                    Q:<input class="question" type="text" name="q3:2"><br>
+                    A:<input class="answer" type="text" name="a3:2">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Blue 300:</div>
+                    Q:<input class="question" type="text" name="q3:3"><br>
+                    A:<input class="answer" type="text" name="a3:3">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Blue 400:</div>
+                    Q:<input class="question" type="text" name="q3:4"><br>
+                    A:<input class="answer" type="text" name="a3:4">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Blue 500:</div>
+                    Q:<input class="question" type="text" name="q3:5"><br>
+                    A:<input class="answer" type="text" name="a3:5">
+                </div>
+            </div>
+
+            <div class="column" id="Yellow">
+                <h1 id="col4" >Column 4:</h1>
+                <div class="qdiv odd">
+                    <div class="text">Yellow 100:</div>
+                    Q:<input class="question" type="text" name="q4:1"><br>
+                    A:<input class="answer" type="text" name="a4:1">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Yellow 200:</div>
+                    Q:<input class="question" type="text" name="q4:2"><br>
+                    A:<input class="answer" type="text" name="a4:2">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Yellow 300:</div>
+                    Q:<input class="question" type="text" name="q4:3"><br>
+                    A:<input class="answer" type="text" name="a4:3">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Yellow 400:</div>
+                    Q:<input class="question" type="text" name="q4:4"><br>
+                    A:<input class="answer" type="text" name="a4:4">
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Yellow 500:</div>
+                    Q:<input class="question" type="text" name="q4:5"><br>
+                    A:<input class="answer" type="text" name="a4:5">
+                </div>
+            </div>
+
+            <div class="column" id="Red">
+                <h1 id="col5" >Column 5:</h1>
+                <div class="qdiv odd">
+                    <div class="text">Red 100:</div>
+                    Q:<input class="question" type="text" name="q5:1"><br>
+                    A:<input class="answer" type="text" name="a5:1">
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Red 200:</div>
+                    Q:<input class="question" type="text" name="q5:2"><br>
+                    A:<input class="answer" type="text" name="a5:2">
+
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Red 300:</div>
+                    Q:<input class="question" type="text" name="q5:3"><br>
+                    A:<input class="answer" type="text" name="a5:3">
+
+                </div>
+                <div class="qdiv even">
+                    <div class="text">Red 400:</div>
+                    Q:<input class="question" type="text" name="q5:4"><br>
+                    A:<input class="answer" type="text" name="a5:4">
+
+                </div>
+                <div class="qdiv odd">
+                    <div class="text">Red 500:</div>
+                    Q:<input class="question" type="text" name="q5:5"><br>
+                    A:<input class="answer" type="text" name="a5:5">
+
+                </div>
+            </div>
+
+            <input class="question" id="submit" type="submit" name="submit" value="Generate Jeopardy Game!">
+        </form>
+    </body>
+
+</html>
